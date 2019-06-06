ba7fb892f91d961d338330aeb2691d251df34ddc
Yura Solomko <solomkoyv@gmail.com>
2019-06-06 17:05:28 +0300

    замена var на const

--- homework.js
+++ homework.js
@@ -1,4 +1,4 @@
-var employers = [
+const employers = [
   "Alex",
   "",
   "ludmila",
@@ -12,17 +12,17 @@ var employers = [
   " Ann"
 ];
 
-var employersNames = [];
-for (var i = 0; i < employers.length; i++) {
+const employersNames = [];
+for (const i = 0; i < employers.length; i++) {
   if (employers[i].length > 0 && employers[i].length != "") {
     employersNames.push(employers[i]);
   }
 }
-for (var i = 0; i < employersNames.length; i++) {
+for (const i = 0; i < employersNames.length; i++) {
   employersNames[i] = employersNames[i].toLowerCase().trim();
 }
 
-var sponsors = {
+const sponsors = {
   cash: [40000, 5000, 30400, 12000],
   eu: ["SRL", "PLO", "J&K"],
   rus: ["RusAuto", "SBO"]
@@ -30,19 +30,19 @@ var sponsors = {
 
 function calcCash(own) {
   own = own || 0;
-  var everyCash = Array.prototype.slice.call(arguments);
-  var total = own;
-  for (var i = 0; i < everyCash[1].length; i++) {
+  const everyCash = Array.prototype.slice.call(arguments);
+  const total = own;
+  for (const i = 0; i < everyCash[1].length; i++) {
     total += +everyCash[1][i];
   }
   return total;
 }
 
-var money = calcCash(null, sponsors.cash);
+const money = calcCash(null, sponsors.cash);
 
 function makeBusiness(owner, director, cash, emp) {
   director = director || "Victor";
-  var sumSponsors = sponsors.eu.concat(sponsors.rus, "unexpected sponsor");
+  const sumSponsors = sponsors.eu.concat(sponsors.rus, "unexpected sponsor");
   console.log(
     "We have a business. Owner: " +
       owner +
